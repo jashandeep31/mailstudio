67f1337569ab2d1f7b1460a5c98c29b0d07ca781
Jashandeep Singh <jashandeep1659@gmail.com>
2026-01-05 20:25:51 +0530

    fix:socket repsonse

--- apps/api/src/web-sockets/handlers/refine-template-event.ts
+++ apps/api/src/web-sockets/handlers/refine-template-event.ts
@@ -8,10 +8,11 @@ import {
 } from "@repo/db";
 import { SocketEventSchemas } from "@repo/shared";
 import z from "zod";
-import { v4 as uuid } from "uuid";
+import { v4 as uuid, version } from "uuid";
 import { refineMailTemplate } from "../../ai/mail/refine-template/index.js";
 import mjml2html from "mjml";
 import { WebSocket } from "ws";
+import { ka } from "zod/locales";
 interface RefineTemplateHandler {
   data: z.infer<(typeof SocketEventSchemas)["event:refine-template-message"]>;
   socket: WebSocket;
@@ -22,6 +23,37 @@ export const refineTemplateHandler = async ({
 }: RefineTemplateHandler) => {
   const versionId = uuid();
   const questionId = uuid();
+  const [preVersionsCount] = await db
+    .select({
+      count: count(),
+    })
+    .from(chatVersionsTable)
+    .where(eq(chatVersionsTable.chat_id, data.chatId));
+  const DummyVersion: typeof chatVersionsTable.$inferSelect = {
+    id: versionId,
+    chat_id: data.chatId,
+    version_number: preVersionsCount ? preVersionsCount.count + 1 : 1,
+    created_at: new Date(),
+    updated_at: new Date(),
+  };
+  const DummyQuestion: typeof chatVersionPromptsTable.$inferSelect = {
+    id: questionId,
+    version_id: versionId,
+    prompt: data.message,
+    brand_kit_id: null,
+    created_at: new Date(),
+  };
+  socket.send(
+    JSON.stringify({
+      key: "res:new-version",
+      data: {
+        chat_versions: DummyVersion,
+        chat_version_prompts: DummyQuestion,
+        chat_version_outputs: {},
+      },
+    }),
+  );
+
   const [refinedMJMLResponse] = await Promise.all([
     await refineMailTemplate({
       prevMjmlCode: "",
@@ -42,17 +74,14 @@ export const refineTemplateHandler = async ({
       const [chatVersion] = await tx
         .insert(chatVersionsTable)
         .values({
-          chat_id: data.chatId,
-          version_number: preVersionsCount ? preVersionsCount.count + 1 : 1,
+          ...DummyVersion,
         })
         .returning();
       if (!chatVersion) throw new Error("failed to create the chat version");
       const [chatQuestion] = await tx
         .insert(chatVersionPromptsTable)
         .values({
-          version_id: chatVersion.id,
-          prompt: data.message,
-          brand_kit_id: null,
+          ...DummyQuestion,
         })
         .returning();
       const [chatOutput] = await tx
