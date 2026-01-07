32760e6fa8fc3507e4e72e8e80da1f8155ec1fe4
Jashandeep Singh <jashandeep1659@gmail.com>
2026-01-07 19:10:31 +0530

    feat: login page

--- apps/web/app/(auth)/login/page.tsx
+++ apps/web/app/(auth)/login/page.tsx
@@ -1,39 +1,41 @@
-import { Button } from "@repo/ui/components/button";
+import GoogleLoginButton from "@/components/google-login-button";
+import { Mail } from "lucide-react";
+import Link from "next/link";
 import React from "react";
-export default function page() {
+
+export default function LoginPage() {
   return (
-    <div className="grid min-h-screen lg:grid-cols-2">
-      <div className="hidden lg:block"></div>
-      <div className="flex items-center justify-center">
-        <div className="min-w-[50%]">
-          <h1 className="mb-8 text-2xl font-bold">Mail Studio</h1>
-          <div>
-            <Button
-              variant="outline"
-              className="w-full justify-start gap-3 border-gray-300 bg-white py-6 font-bold text-gray-700 hover:bg-gray-50"
-            >
-              <svg className="h-5 w-5" viewBox="0 0 24 24">
-                <path
-                  fill="currentColor"
-                  d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"
-                />
-                <path
-                  fill="#34A853"
-                  d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z"
-                />
-                <path
-                  fill="#FBBC05"
-                  d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z"
-                />
-                <path
-                  fill="#EA4335"
-                  d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"
-                />
-              </svg>
-              Login with Google
-            </Button>
+    <div className="grid min-h-svh lg:grid-cols-2">
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="flex justify-center gap-2 md:justify-start">
+          <Link href="/" className="flex items-center gap-2 font-medium">
+            <div className="bg-primary text-primary-foreground flex h-6 w-6 items-center justify-center rounded-md">
+              <Mail className="size-4" />
+            </div>
+            Mail Studio
+          </Link>
+        </div>
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-xs">
+            <div className="flex flex-col gap-6">
+              <div className="flex flex-col gap-2 text-center">
+                <h1 className="text-2xl font-bold">Welcome back</h1>
+                <p className="text-muted-foreground text-sm text-balance">
+                  Login with your Google account
+                </p>
+              </div>
+              <GoogleLoginButton />
+            </div>
           </div>
         </div>
+      </div>
+      <div className="bg-muted relative hidden lg:block">
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          src="https://images.unsplash.com/photo-1596524430615-b46475ddff6e?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="Image"
+          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
       </div>
     </div>
   );
