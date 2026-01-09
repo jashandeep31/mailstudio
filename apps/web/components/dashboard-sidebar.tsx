8c48e54caa3ca3f15c285aeda38f7f8c64c51bfa
Jashandeep Singh <jashandeep1659@gmail.com>
2026-01-09 19:05:07 +0530

    feat: chat rendering

--- apps/web/components/dashboard-sidebar.tsx
+++ apps/web/components/dashboard-sidebar.tsx
@@ -1,20 +1,97 @@
+"use client";
+import { useChats } from "@/hooks/use-chats";
 import {
   Sidebar,
   SidebarContent,
   SidebarFooter,
   SidebarGroup,
+  SidebarGroupLabel,
   SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
 } from "@repo/ui/components/sidebar";
+import { LucideIcon, Search, Home, Sparkles, Inbox } from "lucide-react";
+import Link from "next/link";
+const mainNav = [
+  {
+    title: "Search Templates",
+    url: "#",
+    icon: Search,
+  },
+  {
+    title: "My Templates",
+    url: "#",
+    icon: Sparkles,
+  },
+  {
+    title: "Broadcast",
+    url: "#",
+    icon: Home,
+  },
+  {
+    title: "Marketplace",
+    url: "#",
+    icon: Inbox,
+    badge: "10",
+  },
+];
 
 export function DashboardSidebar() {
+  const chatsRes = useChats();
   return (
-    <Sidebar className="border-0 group-data-[side=left]:border-0">
-      <SidebarHeader />
+    <Sidebar className="mt-16 border-0 group-data-[side=left]:border-0">
+      <SidebarHeader className="bg-background">
+        <MainNav items={mainNav} />
+      </SidebarHeader>
       <SidebarContent className="bg-background">
-        <SidebarGroup />
-        <SidebarGroup />
+        <SidebarGroup>
+          <SidebarGroupLabel>Chats</SidebarGroupLabel>
+
+          <SidebarMenu>
+            {chatsRes.data?.map((chat) => (
+              <SidebarMenuItem key={chat.id}>
+                <SidebarMenuButton
+                  asChild
+                  className="active:bg-background data-active:bg-background bg-red-100"
+                >
+                  <Link href={`/chat/${chat.id}`}>{chat.name}</Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            ))}
+          </SidebarMenu>
+        </SidebarGroup>
       </SidebarContent>
       <SidebarFooter />
     </Sidebar>
   );
 }
+
+const MainNav = ({
+  items,
+}: {
+  items: {
+    title: string;
+    url: string;
+    icon: LucideIcon;
+    isActive?: boolean;
+  }[];
+}) => {
+  return (
+    <SidebarMenu>
+      {items.map((item) => (
+        <SidebarMenuItem key={item.title} className="">
+          <SidebarMenuButton
+            asChild
+            className="active:bg-background data-active:bg-background bg-red-100"
+          >
+            <a href={item.url}>
+              <item.icon />
+              <span>{item.title}</span>
+            </a>
+          </SidebarMenuButton>
+        </SidebarMenuItem>
+      ))}
+    </SidebarMenu>
+  );
+};
---
+++ apps/web/services/chat-services.ts
@@ -0,0 +1,12 @@
+import { BASE_URL } from "@/lib/contants";
+import { chatsTable } from "@repo/db";
+import axios from "axios";
+
+export const getChats = async (): Promise<
+  (typeof chatsTable.$inferSelect)[]
+> => {
+  const res = await axios.get(`${BASE_URL}/api/v1/chats`, {
+    withCredentials: true,
+  });
+  return res.data.data;
+};
